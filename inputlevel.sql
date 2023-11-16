6a00b77050149669ff87d1d33c9cb8a55a82ff2a
ariyanidesigny <148053598+ariyanidesiit@users.noreply.github.com>
2023-11-16 09:49:19 +0700

    Add files via upload
    Signed-off-by: ariyanidesigny <148053598+ariyanidesiit@users.noreply.github.com>

---
+++ inputlevel.sql
@@ -0,0 +1,149 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : db_crud
+ Source Server Type    : MySQL
+ Source Server Version : 80030 (8.0.30)
+ Source Host           : localhost:3306
+ Source Schema         : inputlevel
+
+ Target Server Type    : MySQL
+ Target Server Version : 80030 (8.0.30)
+ File Encoding         : 65001
+
+ Date: 16/11/2023 09:47:33
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for akses
+-- ----------------------------
+DROP TABLE IF EXISTS `akses`;
+CREATE TABLE `akses`  (
+  `id_akses` int NOT NULL AUTO_INCREMENT,
+  `id_roles` int NULL DEFAULT NULL,
+  `id_menu` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id_akses`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of akses
+-- ----------------------------
+INSERT INTO `akses` VALUES (1, 1, 1);
+
+-- ----------------------------
+-- Table structure for datainput
+-- ----------------------------
+DROP TABLE IF EXISTS `datainput`;
+CREATE TABLE `datainput`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `keterangan` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of datainput
+-- ----------------------------
+INSERT INTO `datainput` VALUES (6, 'Yusuf Andi Nugroho', 'Guru');
+INSERT INTO `datainput` VALUES (7, 'Desi Ariyani', 'Guru ');
+
+-- ----------------------------
+-- Table structure for datanilai
+-- ----------------------------
+DROP TABLE IF EXISTS `datanilai`;
+CREATE TABLE `datanilai`  (
+  `id_nilai` int NOT NULL AUTO_INCREMENT,
+  `mapel` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `mapel_nilai` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_nilai`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of datanilai
+-- ----------------------------
+INSERT INTO `datanilai` VALUES (8, 'Bahasa Indonesia', '75');
+
+-- ----------------------------
+-- Table structure for login
+-- ----------------------------
+DROP TABLE IF EXISTS `login`;
+CREATE TABLE `login`  (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `password` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `id_roles` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id_user`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of login
+-- ----------------------------
+INSERT INTO `login` VALUES (10, 'admin', '123', 2);
+INSERT INTO `login` VALUES (11, 'guru', '234', 1);
+
+-- ----------------------------
+-- Table structure for menu
+-- ----------------------------
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE `menu`  (
+  `id_menu` int NOT NULL AUTO_INCREMENT,
+  `nama_menu` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `nama_url` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `nama_icon` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_menu`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of menu
+-- ----------------------------
+INSERT INTO `menu` VALUES (1, 'Data login', 'Datalogin', 'fas fa-fw fa-cog');
+INSERT INTO `menu` VALUES (3, 'Guru', 'guru', 'gdgrth');
+INSERT INTO `menu` VALUES (4, 'Data Mapel & Nilai', 'menu', 'xxxx');
+
+-- ----------------------------
+-- Table structure for roles
+-- ----------------------------
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles`  (
+  `id_roles` int NOT NULL AUTO_INCREMENT,
+  `nama_roles` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_roles`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of roles
+-- ----------------------------
+INSERT INTO `roles` VALUES (1, 'guru');
+INSERT INTO `roles` VALUES (2, 'admin');
+
+-- ----------------------------
+-- Table structure for siswa
+-- ----------------------------
+DROP TABLE IF EXISTS `siswa`;
+CREATE TABLE `siswa`  (
+  `id_siswa` int NOT NULL AUTO_INCREMENT,
+  `nama_siswa` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `ttl` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `jenkel` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `tingkatan` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `alamat` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `agama` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `umur` char(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_siswa`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of siswa
+-- ----------------------------
+INSERT INTO `siswa` VALUES (4, 'Siska Fadillah', '2002-12-01', 'Perempuan', 'SMP', 'Jlegiwinangun', 'Budha', '22');
+INSERT INTO `siswa` VALUES (6, 'Risma Anggraini', '1989-01-25', 'Perempuan', 'SD', 'Yogyakarta', 'kristen', '22');
+INSERT INTO `siswa` VALUES (7, 'Muhammad Taqi Nabilul Lubab', '2000-01-12', 'laki-laki', 'S1', 'Surakarta', 'Islam', '23');
+INSERT INTO `siswa` VALUES (10, 'Danang Iman Pangestu', '1999-12-13', 'Laki-Laki', 'S1', 'Gombong, Kebumen', 'Islam', '24');
+INSERT INTO `siswa` VALUES (11, 'Lutfi Atmaja', '2001-11-13', 'Perempuan', 'MA', 'Jl Kranggan', 'Kristen', '22');
+INSERT INTO `siswa` VALUES (13, 'Reino Barack', '2001-12-09', 'Laki-Laki', 'D4', 'Jl.Kenanga Jakarta Barat', 'Islam', '22');
+INSERT INTO `siswa` VALUES (14, 'Lunami', '2000-12-09', 'Perempuan', 'SMP', 'Jl. Jlegiwinangun Barat Kilometer 100', 'Islam', '23');
+
+SET FOREIGN_KEY_CHECKS = 1;
